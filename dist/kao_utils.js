7483887e7a1fafa12c5a0b394396bcce49f85f44
Chris Loew <cloew123@gmail.com>
2015-09-28 21:03:09 -0600

    Added intial bower script and kao_utils file

---
+++ dist/kao_utils.js
@@ -0,0 +1,67 @@
+$traceurRuntime.ModuleStore.getAnonymousModule(function() {
+  "use strict";
+  angular.module("kao.utils", []).factory("KaoDefer", function($q) {
+    function KaoDefer() {
+      var defer = $q.defer();
+      defer.promise.success = function(callback) {
+        defer.promise.then(function() {
+          callback.apply(this, arguments);
+        });
+        return defer.promise;
+      };
+      defer.promise.error = function(callback) {
+        defer.promise.then(null, function() {
+          callback.apply(this, arguments);
+        });
+        return defer.promise;
+      };
+      return defer;
+    }
+    return KaoDefer;
+  }).directive("dynamicDirective", function($compile) {
+    return {
+      restrict: "E",
+      replace: true,
+      link: function(scope, element, attrs) {
+        if (attrs.directive) {
+          var dom = "<" + attrs.directive + ">" + "</" + attrs.directive + ">";
+          var el = $compile(dom)(scope);
+          element.append(el);
+        }
+      }
+    };
+  }).directive("transcludePlaceholder", function() {
+    return {
+      restrict: "A",
+      replace: true,
+      controller: function($transclude) {
+        this.$transclude = $transclude;
+      },
+      link: function(scope, element, attrs, controller) {
+        var attach = function(clone) {
+          for (var i = 0; i < clone.length; i++) {
+            var el = angular.element(clone[i]);
+            if (el.attr("fills-transclude") === attrs.transcludePlaceholder) {
+              element.empty();
+              element.append(el);
+            }
+          }
+        };
+        controller.$transclude(function(clone) {
+          attach(clone);
+        });
+      }
+    };
+  }).directive("kaoHeader", function() {
+    return {
+      restrict: "E",
+      replace: true,
+      transclude: true,
+      scope: {headerTitle: "@"},
+      template: "<div class=\"col-md-12 text-center\"> <div class=\"col-md-2\" transclude-placeholder=\"left\"></div> <h1 class=\"col-md-8\">{{headerTitle}}</h1> <div class=\"col-md-2\" transclude-placeholder=\"right\"></div> </div>"
+    };
+  });
+  return {};
+});
+
+//# sourceMappingURL=kao_utils.map
